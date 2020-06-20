3125ce3b929584a43835b6092788f3c1813f7c97
Leonard <leonard@linters.de>
2020-06-20 17:39:23 +0200

    Use shorthands & remove error handling in the request handler

--- tests/uptests.ts
+++ tests/uptests.ts
@@ -9,43 +9,34 @@ async function request(suffix: string = "") {
 }
 
 Deno.test("Is website up and is the content right?", async () => {
-    app.route("/", "GET", (req) => "Hello, World!");
-    const content = await request();
+    app.get("/", (req) => "Hello, World!");
 
+    const content = await request();
     if (content !== "Hello, World!") throw Error("Page isn't up or the content is wrong");
 });
 
 Deno.test("Middlewares working?", async () => {
-    app.route("/", "GET", (req) => "Hello, REPLACE_ME!");
+    app.get("/", (req) => "Hello, REPLACE_ME!");
     app.register((req, respondValue) => {
         return {
             ...respondValue,
             content: respondValue.content?.replace("REPLACE_ME", "Planet") || ""
         };
     });
-    const content = await request();
 
+    const content = await request();
     if (content !== "Hello, Planet!") throw Error("Middlewares don't seem to work");
 });
 
 Deno.test("URL parameters working?", async () => {
-    app.route("/api/:action", "GET", (req) => {
-        if (req.parameters.action !== "hello") throw Error("URL parameters don't seem to work");
-        return "Thanks for the API call!";
-    });
+    app.get("/api/:action", (req) => req.parameters.action);
 
     const content = await request(`/api/hello`);
-    await new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, 250);
-    });
+    if (content !== "hello") throw Error("URL parameters don't seem to work");
 });
 
 Deno.test("URL query decoding working?", async () => {
-    app.get("/search", (req) => {
-        return JSON.stringify(req.query);
-    });
+    app.get("/search", (req) => JSON.stringify(req.query));
 
     const content = await request("/search?q=foo+bar&withCharsThatNeedEscaping=%24%26");
     if (content !== `{"q":"foo bar","withCharsThatNeedEscaping":"$&"}`) throw Error("URL query decoding doesn't seem to work");
@@ -55,7 +46,6 @@ Deno.test("Custom fallback handler working?", async () => {
     app.provideFallback((req) => "Nothing to see here");
 
     const content = await request(`/this_route_doesnt_exist`);
-
     if (content !== "Nothing to see here") throw Error("Custom fallback handlers don't seem to work");
 });
 
@@ -63,7 +53,6 @@ Deno.test("Regex routes working?", async () => {
     app.get(new RegExp("\/hello-world\/(.*)"), (req) => JSON.stringify(req.matches));
 
     const content = await request(`/hello-world/hello/okay`);
-
     if (JSON.parse(content)[0] !== "hello/okay") throw Error("Regex routes don't seem to work");
 });
 
@@ -71,6 +60,5 @@ Deno.test("Regex route priorities working?", async () => {
     app.get("/hello-world/should-trigger-first", (req) => "First!");
 
     const content = await request(`/hello-world/should-trigger-first`);
-
     if (content !== "First!") throw Error("Regex route priorities don't seem to work");
 });
