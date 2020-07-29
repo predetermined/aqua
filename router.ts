47f1b82382f717d2a6a03a5a31848f31b027668d
Leonard <leonard@linters.de>
2020-07-29 21:27:01 +0200

    Add function for finding the right static route

--- router.ts
+++ router.ts
@@ -1,4 +1,4 @@
-import { Route, RegexRoute } from "./aqua.ts";
+import {Route, RegexRoute, StaticRoute} from "./aqua.ts";
 
 export default class Router {
     public static parseRequestPath(url: string) {
@@ -18,5 +18,9 @@ export default class Router {
         return regexRoutes.find((regexRoute: RegexRoute) => {
             return requestedPath.replace(regexRoute.path, "").length === 0;
         });
+    }
+
+    public static findMatchingStaticRoute(requestedPath: string, staticRoutes: StaticRoute[]) {
+        return staticRoutes.find(staticRoute => requestedPath.startsWith(staticRoute.path));
     }
 }
