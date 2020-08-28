a07a82c0518ea1e7f14bb965547cb2702cd7d08d
Leonard Ginters <leonard@linters.de>
2020-08-28 23:43:51 +0200

    Fix Deno not killing the process after running all tests

--- tests/uptests.ts
+++ tests/uptests.ts
@@ -1,23 +1,31 @@
 import Aqua from "../aqua.ts";
 
 const app = new Aqua(4000);
+let registeredTests = 0;
+let solvedTests = 0;
 
 async function request(suffix: string = "", options: any = {}) {
-    const r = await fetch(`http://localhost:4000${suffix}`, options);
+    return await (await fetch(`http://localhost:4000${suffix}`, options)).text();
+}
 
-    return await r.text();
+function registerTest(name: string, fn: () => any) {
+    registeredTests++;
+    Deno.test(name, async () => {
+        await fn();
+        solvedTests++;
+    });
 }
 
 await request();
 
-Deno.test("Is website up and is the content right?", async () => {
+registerTest("Is website up and is the content right?", async () => {
     app.get("/", (req) => "Hello, World!");
 
     const content = await request();
     if (content !== "Hello, World!") throw Error("Page isn't up or the content is wrong");
 });
 
-Deno.test("Middlewares working?", async () => {
+registerTest("Middlewares working?", async () => {
     app.get("/", (req) => "Hello, REPLACE_ME!");
     app.register((req, respondValue) => {
         return {
@@ -30,49 +38,49 @@ Deno.test("Middlewares working?", async () => {
     if (content !== "Hello, Planet!") throw Error("Middlewares don't seem to work");
 });
 
-Deno.test("URL parameters working?", async () => {
+registerTest("URL parameters working?", async () => {
     app.get("/api/:action", (req) => req.parameters.action);
 
     const content = await request(`/api/hello`);
     if (content !== "hello") throw Error("URL parameters don't seem to work");
 });
 
-Deno.test("URL query decoding working?", async () => {
+registerTest("URL query decoding working?", async () => {
     app.get("/search", (req) => JSON.stringify(req.query));
 
     const content = await request("/search?q=foo+bar&withCharsThatNeedEscaping=%24%26");
     if (content !== `{"q":"foo bar","withCharsThatNeedEscaping":"$&"}`) throw Error("URL query decoding doesn't seem to work");
 });
 
-Deno.test("Custom fallback handler working?", async () => {
+registerTest("Custom fallback handler working?", async () => {
     app.provideFallback((req) => "Nothing to see here");
 
     const content = await request(`/this_route_doesnt_exist`);
     if (content !== "Nothing to see here") throw Error("Custom fallback handlers don't seem to work");
 });
 
-Deno.test("Regex routes working?", async () => {
+registerTest("Regex routes working?", async () => {
     app.get(new RegExp("\/hello-world\/(.*)"), (req) => JSON.stringify(req.matches));
 
     const content = await request(`/hello-world/hello/okay`);
     if (JSON.parse(content)[0] !== "hello/okay") throw Error("Regex routes don't seem to work");
 });
 
-Deno.test("Regex route priorities working?", async () => {
+registerTest("Regex route priorities working?", async () => {
     app.get("/hello-world/should-trigger-first", (req) => "First!");
 
     const content = await request(`/hello-world/should-trigger-first`);
     if (content !== "First!") throw Error("Regex route priorities don't seem to work");
 });
 
-Deno.test("Body parsing working if Object converted to JSON string?", async () => {
+registerTest("Body parsing working if Object converted to JSON string?", async () => {
     app.post("/test-json-body-parsing", (req) => req.body.test);
 
     const content = await request(`/test-json-body-parsing`, { method: "post", body: JSON.stringify({ test: "hello" }) });
     if (content !== "hello") throw Error("Body parsing of a object converted to a JSON string don't seem to work");
 });
 
-Deno.test("Body parsing working if passed FormData?", async () => {
+registerTest("Body parsing working if passed FormData?", async () => {
     app.post("/test-formdata-body-parsing", (req) => req.body.test);
 
     const f = new FormData();
@@ -81,3 +89,7 @@ Deno.test("Body parsing working if passed FormData?", async () => {
     const content = await request(`/test-formdata-body-parsing`, { method: "post", body: f });
     if (content !== "hello") throw Error("Body parsing of FormData don't seem to work");
 });
+
+setInterval(() => {
+    if (registeredTests === solvedTests) Deno.exit(0);
+}, 500);
