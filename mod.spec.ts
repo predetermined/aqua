a077b692e9210b91273e50cc99eec08d7056bb21
grayliquid <l@l5d.li>
2023-06-17 19:35:28 +0200

    test: add some basic tests

---
+++ mod.spec.ts
@@ -0,0 +1,151 @@
+import { assert } from "https://deno.land/std@0.192.0/testing/asserts.ts";
+import { FakeAqua, Method, ResponseError } from "./mod.ts";
+
+Deno.test(async function notFound() {
+  const app = new FakeAqua();
+
+  const res = await app.fakeCall(new Request("http://localhost/"));
+
+  assert(res.status === 404);
+  assert((await res.text()) === "Not found.");
+});
+
+Deno.test(async function simpleDELETE() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.DELETE, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(
+    new Request("http://localhost/", {
+      method: "DELETE",
+    })
+  );
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function simpleGET() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.GET, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(new Request("http://localhost/"));
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function simpleOPTIONS() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.OPTIONS, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(
+    new Request("http://localhost/", {
+      method: "OPTIONS",
+    })
+  );
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function simpleHEAD() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.HEAD, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(
+    new Request("http://localhost/", {
+      method: "HEAD",
+    })
+  );
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function simpleOPTIONS() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.PATCH, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(
+    new Request("http://localhost/", {
+      method: "PATCH",
+    })
+  );
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function simplePOST() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.POST, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(
+    new Request("http://localhost/", { method: "POST" })
+  );
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function simplePUT() {
+  const app = new FakeAqua();
+
+  app.route("/").respond(Method.PUT, (_event) => new Response("worked!"));
+
+  const res = await app.fakeCall(
+    new Request("http://localhost/", { method: "PUT" })
+  );
+
+  assert(res.status === 200);
+  assert((await res.text()) === "worked!");
+});
+
+Deno.test(async function addCustomPropertyStep() {
+  const app = new FakeAqua();
+
+  app
+    .route("/")
+    .step((event) => {
+      return {
+        ...event,
+        foo: "bar",
+      };
+    })
+    .respond(Method.GET, (event) => new Response(event.foo));
+
+  const res = await app.fakeCall(new Request("http://localhost/"));
+
+  assert(res.status === 200);
+  assert((await res.text()) === "bar");
+});
+
+Deno.test(async function throwResponseErrorInStep() {
+  const app = new FakeAqua();
+
+  app
+    .route("/")
+    .step((event) => {
+      // just so it doesn't infer `never`
+      if (event.request.headers.has("test")) return event;
+
+      throw new ResponseError(
+        "failed",
+        new Response("failed", {
+          status: 500,
+        })
+      );
+    })
+    .respond(Method.GET, (_event) => new Response("succeeded"));
+
+  const res = await app.fakeCall(new Request("http://localhost/"));
+
+  assert(res.status === 500);
+  assert((await res.text()) === "failed");
+});
